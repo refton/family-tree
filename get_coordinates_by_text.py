2b5c9e733cff0a312ae38ec590821c139eb23a87
gam <iliy-gam@mail.ru>
2022-07-21 11:46:14 +0300

    create get_coordinates_by_text.py

--- get_coordinates_by_text.py
+++ get_coordinates_by_text.py
@@ -1,1 +1,7 @@
 # Created by gam at 20.07.2022
+pip install geopy
+from geopy.geocoders import Nominatim
+loc = Nominatim(user_agent="GetLoc")
+getLoc = loc.geocode("Moscow")
+print("Latitude = ", getLoc.latitude, "\n")
+print("Longitude = ", getLoc.longitude)
